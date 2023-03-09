5e6737d8f60e88290b824192dbb85618ac53b4a6
homerabbit <me92years@gmail.com>
2023-03-10 01:49:27 +0900

    update Local

--- frontend/src/Setting.js
+++ frontend/src/Setting.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const LOCAL = "http://localhost:8080";
+export const LOCAL = "http://52.79.203.163";
 export const api = axios.create({
   baseURL: LOCAL,
   withCredentials: true
