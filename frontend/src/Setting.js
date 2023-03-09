59960aa5d545d0c337fe56bf24798e572b6b2564
homerabbit <me92years@gmail.com>
2023-03-09 23:23:37 +0900

    LOCAL server port update

--- frontend/src/Setting.js
+++ frontend/src/Setting.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const LOCAL = "http://localhost:80";
+export const LOCAL = "http://localhost:8080";
 export const api = axios.create({
   baseURL: LOCAL,
   withCredentials: true
